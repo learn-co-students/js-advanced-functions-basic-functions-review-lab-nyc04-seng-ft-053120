53cbd33fc294ba4663dfab14a4c5ad44ac85f23b
AlveeM <alvee.akand@outlook.com>
2020-08-08 15:04:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+function wrapAdjective(flair="*") {
+  return function innerFn(adj="special") {
+    return `You are ${flair}${adj}${flair}!`;
+  }
+}
+
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+const Calculator = {
+  add: function add(...args) {
+    return args.reduce((acc, num) => acc += num);
+  },
+  subtract: function subtract(...args) {
+    return args.reduce((acc, num) => acc -= num);
+  },
+  multiply: function multiply(...args) {
+    return args.reduce((acc, num) => acc *= num);
+  },
+  divide: function divide(...args) {
+    return args.reduce((acc, num) => acc /= num);
+  },
+};
+
+function actionApplyer(startInt, fnArr) {
+  return fnArr.reduce((acc, fn) => acc = fn(acc), startInt);
+}
